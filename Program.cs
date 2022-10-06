2ab1e592e95fc6a881dbf725d4f8d97a51b0adc0
Jason D'Oyley <jdfdoyley@gmail.com>
2022-10-06 19:58:40 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿/************************************************************
+ * Name: Jason D'Oyley
+ * Date: Oct. 6, 2022
+ * Assignment CIS214 Week 1 GP - Operations and Data Types
+ *
+ * Main application (program) class.
+ * In this application we will devlare variables of various types, compare
+ * variables using comparison operators, get numberic values from the user
+ * via the console, add the numbers received, and print the results of
+ * each operation performed.
+ */
---
+++ README.md
@@ -0,0 +1,3 @@
+# Operators and Data Type
+
+In this application we declare variables of various types, compare variables using comparison operators, get numberic values from the user via the console, add the numbers received, and print the result of each operation performed.
